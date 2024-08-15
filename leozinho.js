9fd2d0cb5383157f3dc3f3be7d2085e9a98bb28b
BrasinoBrasil <brasininho@gmail.com>
2024-08-15 16:59:18 -0300

    New file made.

---
+++ leozinho.js
@@ -0,0 +1,9 @@
+const sweethearts = ["Renatinha Rodrigues", "Leonardo", "Arthur", "Ana", "Fernando", "Erick", "Guilherme", "Wellington", "Erick", "João Pedro", "Andrio"]
+class Love {
+    constructor(array){
+        this.array = array;
+    }
+    loves(array){
+        return array;
+    }
+}
